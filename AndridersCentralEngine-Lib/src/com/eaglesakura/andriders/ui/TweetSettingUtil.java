af547d27eb9ae2f5a23ddb590669fc57db44ce7f
eaglesakura <eagle.sakura@gmail.com>
2014-06-14 16:34:15 +0900

    ツイート設定を呼び出し・取り出しのためのUtilを追加

---
+++ AndridersCentralEngine-Lib/src/com/eaglesakura/andriders/ui/TweetSettingUtil.java
@@ -0,0 +1,33 @@
+package com.eaglesakura.andriders.ui;
+
+import android.content.Intent;
+
+import com.eaglesakura.andriders.AceLog;
+import com.eaglesakura.andriders.protocol.CommandProtocol.TweetRequestPayload;
+
+public class TweetSettingUtil {
+
+    /**
+     * ツイート設定用メッセージ
+     */
+    public static final String EXTRA_TWEETREQUEST_PAYLOAD = "EXTRA_TWEETREQUEST_PAYLOAD";
+
+    /**
+     * ツイート設定Activityからツイート設定メッセージを受け取る
+     * @param resultIntent
+     * @return
+     */
+    public static TweetRequestPayload getResultTweetRequest(Intent resultIntent) {
+        byte[] extra = resultIntent.getByteArrayExtra(EXTRA_TWEETREQUEST_PAYLOAD);
+        if (extra == null) {
+            return null;
+        }
+
+        try {
+            return TweetRequestPayload.parseFrom(extra);
+        } catch (Exception e) {
+            AceLog.d(e);
+            return null;
+        }
+    }
+}
