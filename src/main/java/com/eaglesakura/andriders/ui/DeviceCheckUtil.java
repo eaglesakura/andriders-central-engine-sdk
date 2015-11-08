b8bc44dd6f109f5c3c845d9dee78b1eab2fe5f23
eaglesakura <eagle.sakura@gmail.com>
2015-11-08 23:43:39 +0900

    環境設定用Utilクラスを追加

---
+++ src/main/java/com/eaglesakura/andriders/ui/DeviceCheckUtil.java
@@ -0,0 +1,44 @@
+package com.eaglesakura.andriders.ui;
+
+
+import android.content.Context;
+
+public class DeviceCheckUtil {
+
+    /**
+     * 端末にACEがインストール済みであればtrueを返却する
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isAceInstalled(Context context) {
+        return false;
+    }
+
+    /**
+     * ACEのインストールを促すダイアログを表示する
+     *
+     * @param context
+     */
+    public static void showAceInstallDialog(Context context) {
+
+    }
+
+    /**
+     * ACEが起動中であればtrueを返却する
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isAceBooted(Context context) {
+        return false;
+    }
+
+    /**
+     * ACEの起動を促すダイアログを表示する
+     *
+     * @param context
+     */
+    public static void showAceServiceBootDialog(Context context) {
+    }
+}
