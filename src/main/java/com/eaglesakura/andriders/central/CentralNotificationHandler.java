7d3a266185e1dbab312427f5b8a67537c1affd26
eaglesakura <eagle.sakura@gmail.com>
2016-07-18 17:34:12 +0900

    import整理

--- src/main/java/com/eaglesakura/andriders/central/CentralNotificationHandler.java
+++ src/main/java/com/eaglesakura/andriders/central/CentralNotificationHandler.java
@@ -1,7 +1,6 @@
 package com.eaglesakura.andriders.central;
 
 import com.eaglesakura.andriders.command.CommandKey;
-import com.eaglesakura.andriders.command.SerializableIntent;
 import com.eaglesakura.andriders.serialize.NotificationProtocol;
 import com.eaglesakura.andriders.serialize.RawIntent;
 
