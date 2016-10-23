085b4376b067c6deae8a5b303e3482fd55b402c0
eaglesakura <eagle.sakura@gmail.com>
2016-10-23 11:07:37 +0900

    UnitTestのRunnerを調整

--- src/test/java/com/eaglesakura/andriders/UnitTestCase.java
+++ src/test/java/com/eaglesakura/andriders/UnitTestCase.java
@@ -3,11 +3,9 @@ package com.eaglesakura.andriders;
 import com.eaglesakura.andriders.sdk.BuildConfig;
 import com.eaglesakura.android.AndroidSupportTestCase;
 
-import org.junit.runner.RunWith;
-
 import org.robolectric.annotation.Config;
 
-@RunWith(RobolectricGradleLibraryTestRunner.class)
-@Config(constants = BuildConfig.class, packageName = BuildConfig.APPLICATION_ID, sdk = 21, manifest = "src/main/AndroidManifest.xml")
+//@RunWith(RobolectricGradleLibraryTestRunner.class)
+@Config(constants = BuildConfig.class, packageName = BuildConfig.APPLICATION_ID, sdk = 22, manifest = "src/main/AndroidManifest.xml")
 public abstract class UnitTestCase extends AndroidSupportTestCase {
 }
