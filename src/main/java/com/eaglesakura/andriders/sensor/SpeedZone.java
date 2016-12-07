c57b77ccd9a1df827f035a8c36533af70c963944
eaglesakura <eagle.sakura@gmail.com>
2016-12-07 12:26:10 +0900

    Speed/Cadenceのゾーン名テキストを追加

--- src/main/java/com/eaglesakura/andriders/sensor/SpeedZone.java
+++ src/main/java/com/eaglesakura/andriders/sensor/SpeedZone.java
@@ -1,8 +1,44 @@
 package com.eaglesakura.andriders.sensor;
 
+import com.eaglesakura.andriders.sdk.R;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
 public enum SpeedZone {
-    Stop,
-    Slow,
-    Cruise,
-    Sprint,
+    Stop {
+        @NonNull
+        @Override
+        public String getDisplayText(@NonNull Context context) {
+            return context.getString(R.string.Ace_Word_SpeedZone_Stop);
+        }
+    },
+    Slow {
+        @NonNull
+        @Override
+        public String getDisplayText(@NonNull Context context) {
+            return context.getString(R.string.Ace_Word_SpeedZone_Slow);
+        }
+    },
+    Cruise {
+        @NonNull
+        @Override
+        public String getDisplayText(@NonNull Context context) {
+            return context.getString(R.string.Ace_Word_SpeedZone_Cruise);
+        }
+    },
+    Sprint {
+        @NonNull
+        @Override
+        public String getDisplayText(@NonNull Context context) {
+            return context.getString(R.string.Ace_Word_SpeedZone_Sprint);
+        }
+    };
+
+
+    /**
+     * 表示用のテキストを取得する
+     */
+    @NonNull
+    public abstract String getDisplayText(@NonNull Context context);
 }
