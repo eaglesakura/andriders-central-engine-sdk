f372afd60fbec863e83dab23d40cf2661c2230be
eaglesakura <eagle.sakura@gmail.com>
2016-12-07 09:47:09 +0900

    string.xmlを整理

--- src/main/java/com/eaglesakura/andriders/sensor/HeartrateZone.java
+++ src/main/java/com/eaglesakura/andriders/sensor/HeartrateZone.java
@@ -1,33 +1,81 @@
 package com.eaglesakura.andriders.sensor;
 
+import com.eaglesakura.andriders.sdk.R;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
 public enum HeartrateZone {
+
     /**
      * 安静
      */
-    Repose,
+    Repose {
+        @NonNull
+        @Override
+        public String getDisplayText(@NonNull Context context) {
+            return context.getString(R.string.Ace_Word_HeartrateZone_Repose);
+        }
+    },
 
     /**
      * 軽度
      */
-    Easy,
+    Easy {
+        @NonNull
+        @Override
+        public String getDisplayText(@NonNull Context context) {
+            return context.getString(R.string.Ace_Word_HeartrateZone_Easy);
+        }
+    },
 
     /**
      * 脂肪燃焼
      */
-    FatCombustion,
+    FatCombustion {
+        @NonNull
+        @Override
+        public String getDisplayText(@NonNull Context context) {
+            return context.getString(R.string.Ace_Word_HeartrateZone_FatCombustion);
+        }
+    },
 
     /**
      * 有酸素
      */
-    PossessionOxygenMotion,
+    PossessionOxygenMotion {
+        @NonNull
+        @Override
+        public String getDisplayText(@NonNull Context context) {
+            return context.getString(R.string.Ace_Word_HeartrateZone_PossessionOxygenMotion);
+        }
+    },
 
     /**
      * 無酸素
      */
-    NonOxygenatedMotion,
+    NonOxygenatedMotion {
+        @NonNull
+        @Override
+        public String getDisplayText(@NonNull Context context) {
+            return context.getString(R.string.Ace_Word_HeartrateZone_NonOxygenatedMotion);
+        }
+    },
 
     /**
      * 危険域
      */
-    Overwork,
+    Overwork {
+        @NonNull
+        @Override
+        public String getDisplayText(@NonNull Context context) {
+            return context.getString(R.string.Ace_Word_HeartrateZone_Overwork);
+        }
+    };
+
+    /**
+     * 表示用のテキストを取得する
+     */
+    @NonNull
+    public abstract String getDisplayText(@NonNull Context context);
 }
