48b856d19f98fa8b2d6fd93be82544593e27e5e1
eaglesakura <eagle.sakura@gmail.com>
2016-12-31 23:52:45 +0900

    GPXパーサーでGMT時刻インポートを行うように修正

--- src/test/java/com/eaglesakura/andriders/data/gpx/GpxParserTest.java
+++ src/test/java/com/eaglesakura/andriders/data/gpx/GpxParserTest.java
@@ -2,10 +2,12 @@ package com.eaglesakura.andriders.data.gpx;
 
 import com.eaglesakura.andriders.UnitTestCase;
 import com.eaglesakura.util.DateUtil;
-import com.eaglesakura.util.IOUtil;
 import com.eaglesakura.util.LogUtil;
 
 import org.junit.Test;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -16,32 +18,38 @@ import java.util.TimeZone;
 public class GpxParserTest extends UnitTestCase {
 
     @Test
-    public void GPX書式の時刻が正しくパースできる() throws Exception {
+    public void GPX書式の時刻が正しくパースできる_形式A() throws Exception {
         String time = "2015-05-23T20:51:28Z";
         Date date = GpxParser.sGpxDateFormat.parse(time);
-        date = new Date(date.getTime() + DateUtil.getDateOffset());
 
         // AACR2015の朝である
         LogUtil.log("Date[%s]", date.toString());
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeZone(TimeZone.getDefault());
-        calendar.setTime(date);
-        assertEquals(DateUtil.getYear(calendar), 2015);
-        assertEquals(DateUtil.getMonth(calendar), 5);
-        assertEquals(DateUtil.getDay(calendar), 24);
-        assertEquals(DateUtil.getHour(calendar), 5);
+        assertEquals(DateUtil.getYear(date, TimeZone.getDefault()), 2015);
+        assertEquals(DateUtil.getMonth(date, TimeZone.getDefault()), 5);
+        assertEquals(DateUtil.getDay(date, TimeZone.getDefault()), 24);
+        assertEquals(DateUtil.getHour(date, TimeZone.getDefault()), 5);
     }
 
     @Test
+    public void GPX書式の時刻が正しくパースできる_形式B() throws Exception {
+        String time = "2016-05-21T20:51:11.000Z";
+        Date date = GpxParser.sGpxDateFormat2.parse(time);
+
+        // AACR2016の朝である
+        LogUtil.log("Date[%s]", date.toString());
+        assertEquals(DateUtil.getYear(date, TimeZone.getDefault()), 2016);
+        assertEquals(DateUtil.getMonth(date, TimeZone.getDefault()), 5);
+        assertEquals(DateUtil.getDay(date, TimeZone.getDefault()), 22);
+        assertEquals(DateUtil.getHour(date, TimeZone.getDefault()), 5);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Test
     public void AACRのGPXが正しくパースできる() throws Exception {
         GpxParser parser = new GpxParser();
-        InputStream is = new FileInputStream(getTestAsset("gpx/sample-aacr2015.gpx"));
         Gpx gpx;
-        try {
-            parser.setDateOption(GpxParser.DateOption.AddTimeZone);
+        try (InputStream is = new FileInputStream(getTestAsset("gpx/sample-aacr2015.gpx"))) {
             gpx = parser.parse(is);
-        } finally {
-            IOUtil.close(is);
         }
 
         assertNotNull(gpx);
