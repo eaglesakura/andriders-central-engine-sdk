991e8fd2d8409147a896e5d707c66b072bfed1f8
eaglesakura <eaglesakura@SQUALL.local>
2017-08-05 11:58:41 +0900

    AS 3.0 P9対応

--- src/test/java/com/eaglesakura/andriders/BuildConfigTest.java
+++ src/test/java/com/eaglesakura/andriders/BuildConfigTest.java
@@ -5,11 +5,11 @@ import com.eaglesakura.andriders.sdk.R;
 import com.eaglesakura.thread.Holder;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 public class BuildConfigTest extends UnitTestCase {
     @Test
     public void BuildConfig出力が行えている() throws Exception {
-        assertNotEmpty(getContext().getString(R.string.Ace_Word_HeartrateZone_NonOxygenatedMotion));
         assertNotEmpty(BuildConfig.ACE_SDK_VERSION);
     }
 
@@ -18,9 +18,7 @@ public class BuildConfigTest extends UnitTestCase {
         Holder<Boolean> holder = new Holder<>();
         holder.set(Boolean.FALSE);
 
-        Runnable call = () -> {
-            holder.set(Boolean.TRUE);
-        };
+        Runnable call = () -> holder.set(Boolean.TRUE);
 
         call.run();
 
--- src/test/java/com/eaglesakura/andriders/UnitTestCase.java
+++ src/test/java/com/eaglesakura/andriders/UnitTestCase.java
@@ -3,9 +3,11 @@ package com.eaglesakura.andriders;
 import com.eaglesakura.andriders.sdk.BuildConfig;
 import com.eaglesakura.android.AndroidSupportTestCase;
 
+import org.junit.runner.RunWith;
+
 import org.robolectric.annotation.Config;
 
-@Config(constants = BuildConfig.class, packageName = BuildConfig.APPLICATION_ID, sdk = 23, manifest = "src/main/AndroidManifest.xml")
+@Config(constants = BuildConfig.class, packageName = BuildConfig.APPLICATION_ID, sdk = 23)
 public abstract class UnitTestCase extends AndroidSupportTestCase {
     protected String TAG = getClass().getSimpleName();
 }
